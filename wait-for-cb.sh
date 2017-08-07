12dfb9d4bdcb5c4a36339be3da302fc4bd0ea7d5
Diego Silveira <diegossilveira@gmail.com>
2017-08-07 19:37:31 -0300

    Adding support to design_doc status probing

--- wait-for-cb.sh
+++ wait-for-cb.sh
@@ -5,16 +5,17 @@ wait_for_couchbase()
    for (( i=1; i<=$ATTEMPTS; i++ ))
    do
       print_progress $i
-      result=$(check_bucket_status)
-      if [[ $result == "healthy" ]]
+      check
+      if [[ $? -eq 0 ]]
       then
-         echo -e "\nBucket ${BUCKET} is healthy"
+         echo -e "\nReady!"
          run_command
          exit $?
       fi
       sleep 1
    done
    echo -e "\nMax attempts reached."
+   exit 1
 }
 
 print_progress()
@@ -27,12 +28,29 @@ print_progress()
    echo -ne "] $1/$ATTEMPTS\r"
 }
 
+check()
+{
+  if [[ -z "$DESIGN_DOC" ]]
+  then
+     check_bucket_status
+  else
+     check_design_doc_status
+  fi
+}
+
 check_bucket_status()
 {
    url="http://${HOST}:${PORT}/pools/default/buckets/${BUCKET}"
    credentials="${USER}:${PASSWORD}"
    result=`curl -s -u $credentials $url | $JS_AWK 'return this.nodes[0].status' 2> /dev/null`
-   echo $result
+   [ "$result" = "healthy" ]
+}
+
+check_design_doc_status()
+{
+   url="http://${HOST}:${PORT}/${BUCKET}/_design/${DESIGN_DOC}"
+   credentials="${USER}:${PASSWORD}"
+   curl -s -f -u $credentials $url > /dev/null
 }
 
 run_command()
@@ -48,14 +66,15 @@ usage()
    cat << USAGE >&2
 Usage:
    $(basename $0) [options] [-- COMMAND]
-   -h, --host HOST            Couchbase host or IP
-   -p, --port PORT            Couchbase TCP port
-   -b, --bucket BUCKET_NAME   Couchbase bucket name
-   -u, --user USER            Couchbase admin username
-   -P, --password PASSWORD    Couchbase admin password
-   -a, --jsawk JSAWK_PATH     jsawk binary path. Defaults to $PATH/jsawk
-   -j, --js JS_PATH           js engine binary path (e.g. spidermonkey). Defaults to $PATH/js
-   -t, --attempts ATTEMPTS    Attempts before failing. Defaults to 3
+   -h, --host HOST                    Couchbase host or IP
+   -p, --port PORT                    Couchbase TCP port
+   -b, --bucket BUCKET_NAME           Couchbase bucket name
+   -d, --design-doc DESIGN_DOC_NAME   Design Document name
+   -u, --user USER                    Couchbase admin username
+   -P, --password PASSWORD            Couchbase admin password
+   -a, --jsawk JSAWK_PATH             jsawk binary path. Defaults to $PATH/jsawk
+   -j, --js JS_PATH                   js engine binary path (e.g. spidermonkey). Defaults to $PATH/js
+   -t, --attempts ATTEMPTS            Attempts before failing. Defaults to 3
 USAGE
    exit 1
 }
@@ -83,6 +102,10 @@ parse_arguments()
          ;;
          -b | --bucket)
          BUCKET="$2"
+         shift 2
+         ;;
+         -d | --design-doc)
+         DESIGN_DOC="$2"
          shift 2
          ;;
          -u | --user)
@@ -114,46 +137,46 @@ parse_arguments()
 
 initialize_arguments()
 {
-   if [[ "$HOST" == "" ]]
+   if [[ -z "$HOST" ]]
    then
       echoerr "Error: you need to provide a couchbase host."
       usage
    fi
 
-   if [[ "$BUCKET" == "" ]]
+   if [[ -z "$BUCKET" ]]
    then
       echoerr "Error: you need to provide a couchbase bucket name."
       usage
    fi
 
-   if [[ "$USER" == "" ]]
+   if [[ -z "$USER" ]]
    then
       echoerr "Error: you need to provide a couchbase user."
       usage
    fi
 
-   if [[ "$PASSWORD" == "" ]]
+   if [[ -z "$PASSWORD" ]]
    then
       echoerr "Error: you need to provide a couchbase password."
       usage
    fi
 
-   if [[ "$PORT" == "" ]]
+   if [[ -z "$PORT" ]]
    then
       PORT="8091"
    fi
 
-   if [[ "$JS_AWK" == "" ]]
+   if [[ -z "$JS_AWK" ]]
    then
       JS_AWK="jsawk"
    fi
 
-   if [[ "$JS" == "" ]]
+   if [[ -z "$JS" ]]
    then
      JS="js"
    fi
 
-   if [[ "$ATTEMPTS" == "" ]]
+   if [[ -z "$ATTEMPTS" ]]
    then
      ATTEMPTS=3
    fi
